fc7466c61d0b2dfd94566c248acc65909aadc743
Karan Talreja <karantalreja91@gmail.com>
2014-12-06 01:01:08 +0530

    Added run from command line support by adding directory to syspath Updated gitignore to ignore image files

--- SVM_Deployment/SVM_Deployment.py
+++ SVM_Deployment/SVM_Deployment.py
@@ -1,5 +1,5 @@
 import sys
-from class_vis import prettyPicture
+sys.path.append("../GaussianDeployment/")
 from prep_terrain_data import makeTerrainData
 
 import matplotlib.pyplot as plt
@@ -14,7 +14,7 @@ features_train, labels_train, features_test, labels_test = makeTerrainData()
 ########################## SVM #################################
 ### we handle the import statement and SVC creation for you here
 from sklearn.svm import SVC
-clf = SVC(kernel="linear")
+clf = SVC(kernel="rbf",C=100.0,gamma=100.0)
 
 
 #### now your job is to fit the classifier
@@ -31,6 +31,12 @@ pred = clf.predict(features_test);
 from sklearn.metrics import accuracy_score
 acc = accuracy_score(pred, labels_test)
 print "Accuracy: ",acc
+
+    ### draw the decision boundary with the text points overlaid
+
+from class_vis import prettyPicture, output_image
+prettyPicture(clf, features_test, labels_test)
+output_image("test.png", "png", open("test.png", "rb").read())
 
 def submitAccuracy():
     return acc
